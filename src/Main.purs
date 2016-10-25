e4980c0a80930d73ade1853bcf6de04bf026db05
Liam Goodacre <goodacre.liam@gmail.com>
2016-10-25 22:17:38 +0100

    Computing types
    Remove unnecessary data constructors
    Add expression terms for Type, Type1, and AppType1
    Add evaluation rules for new terms
    Add RunType, runType, RunSymbol, and rename runSymbol.
    Add example with list of Type1, folding with AppType1, and using
    runType.

--- src/Main.purs
+++ src/Main.purs
@@ -3,6 +3,7 @@ module Main where
 import Prelude
 import Type.Proxy (Proxy(..))
 import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol, reifySymbol)
+import Data.Maybe (Maybe(..))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
@@ -11,11 +12,11 @@ import Control.Monad.Eff.Console (CONSOLE, log)
 
 class Nat d
 
-data Zero = NatZero
+data Zero
 instance natZero
   :: Nat Zero
 
-data Succ n = NatSucc
+data Succ n
 instance natSucc
   :: Nat n => Nat (Succ n)
 
@@ -31,22 +32,22 @@ type Five = Succ Four
 
 class Expr d
 
-data Var n = Var
+data Var n
 instance exprVar
   :: Nat n
   => Expr (Var n)
 
-data Lam b = Lam
+data Lam b
 instance exprLam
   :: Expr b
   => Expr (Lam b)
 
-data App l r = App
+data App l r
 instance exprApp
   :: (Expr l, Expr r)
   => Expr (App l r)
 
-data Str (b :: Symbol) = Str
+data Str (b :: Symbol)
 instance exprStr
   :: Expr (Str b)
 
@@ -54,6 +55,19 @@ data Cat l r
 instance exprCat
   :: (Expr l, Expr r)
   => Expr (Cat l r)
+
+data Type ty
+instance exprType
+  :: Expr (Type ty)
+
+data Type1 (ty1 :: * -> *)
+instance exprType1
+  :: Expr (Type1 ty1)
+
+data AppType1 ty1 ty
+instance exprAppType1
+  :: (Expr ty1, Expr ty)
+  => Expr (AppType1 ty1 ty)
 
 
 -- | Values
@@ -69,6 +83,14 @@ instance valueFunVal
 data StrVal (string :: Symbol)
 instance valueStrVal
   :: Value (StrVal string)
+
+data TypeVal ty
+instance valueTypeVal
+  :: Value (TypeVal ty)
+
+data Type1Val (ty1 :: * -> *)
+instance valueType1Val
+  :: Value (Type1Val ty1)
 
 
 -- | Context
@@ -119,6 +141,20 @@ instance evalApp
       Eval (Extend arg funContext) body output)
   => Eval context (App f x) output
 
+instance evalType
+  :: Context context
+  => Eval context (Type ty) (TypeVal ty)
+
+instance evalType1
+  :: Context context
+  => Eval context (Type1 ty1) (Type1Val ty1)
+
+instance evalAppType1
+  :: (Context context,
+      Eval context f (Type1Val ty1),
+      Eval context x (TypeVal ty))
+  => Eval context (AppType1 f x) (TypeVal (ty1 ty))
+
 instance evalStr
   :: Context context
   => Eval context (Str b) (StrVal b)
@@ -135,18 +171,33 @@ instance isSymbolCatSym :: (IsSymbol l, IsSymbol r) => IsSymbol (CatSym l r) whe
     reflectSymbol (SProxy :: SProxy l) <> reflectSymbol (SProxy :: SProxy r)
 
 
-eval :: forall expr output.
-  (Expr expr,
-   Eval Empty expr (StrVal output)) =>
-  Proxy expr -> SProxy output
-eval Proxy = SProxy
+class RunType expr output | expr -> output
+instance runTypeEval
+  :: Eval Empty expr (TypeVal output) => RunType expr output
 
-evalString :: forall expr output.
+runType :: forall expr output r.
+  (Expr expr,
+   RunType expr output) =>
+  Proxy expr -> (output -> r) -> output -> r
+runType Proxy f = f
+
+
+class RunSymbol expr (output :: Symbol) | expr -> output
+instance runSymbolEval
+  :: Eval Empty expr (StrVal output) => RunSymbol expr output
+
+runSymbol :: forall expr output.
+  (Expr expr,
+   RunSymbol expr output) =>
+  Proxy expr -> SProxy output
+runSymbol Proxy = SProxy
+
+runString :: forall expr output.
   (Expr expr,
    IsSymbol output,
-   Eval Empty expr (StrVal output)) =>
+   RunSymbol expr output) =>
   Proxy expr -> String
-evalString = reflectSymbol <<< eval
+runString = reflectSymbol <<< runSymbol
 
 
 -- | Examples
@@ -162,7 +213,6 @@ type If b t e = App (App b t) e
 type Nil = Lam (Lam (Var Zero))
 type Cons h t = Lam (Lam (App (App (Var One) h) (App (App t (Var One)) (Var Zero))))
 type Fold c n xs = App (App xs c) n
-type Map f xs = Fold (Lam (Lam (Cons (App f (Var One)) (Var Zero)))) Nil xs
 type JoinStrings xs = Fold (Lam (Lam (Cat (Var One) (Var Zero)))) (Str "") xs
 
 type Hello = App Id (Str "Hello")
@@ -173,13 +223,34 @@ type Y = JoinStrings (Cons (Str "A") (Cons X (Cons HelloWorld Nil)))
 
 -- inspect the computed type of `p`
 p :: SProxy _
-p = eval (Proxy :: Proxy Y)
+p = runSymbol (Proxy :: Proxy Y)
+
+
+-- AppNTypes [f, g, ..., h] x -> f (g (... (h x)))
+type AppNTypes xs u = Fold (Lam (Lam (AppType1 (Var One) (Var Zero)))) u xs
+-- CollM = [Maybe, Maybe]
+type CollM = Cons (Type1 Maybe) (Cons (Type1 Maybe) Nil)
+-- CollP = [Proxy, Proxy, Proxy]
+type CollP = Cons (Type1 Proxy) (Cons (Type1 Proxy) (Cons (Type1 Proxy) Nil))
+type MM u = AppNTypes CollM u
+type PPP u = AppNTypes CollP u
+
+-- program building the type `Maybe (Maybe (Proxy (Proxy (Proxy Unit))))`
+maybe2Proxy3Type :: Proxy (MM (PPP (Type Unit)))
+maybe2Proxy3Type = Proxy
+
+-- evaluate maybe2Proxy3Type and use the resulting type
+computedType :: _
+computedType = runType maybe2Proxy3Type (case _ of
+  Nothing -> 0
+  Just Nothing -> 1
+  Just (Just Proxy) -> 2)
+
 
 helloWorld :: String
 helloWorld = reifySymbol "Hello" (\o -> reifySymbol "World" \t -> one o t) where
   one :: forall l r. (IsSymbol l, IsSymbol r) => SProxy l -> SProxy r -> String
-  one _ _ = evalString (Proxy :: Proxy (Cat (Str l) (Str r)))
-
+  one _ _ = runString (Proxy :: Proxy (Cat (Str l) (Str r)))
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
